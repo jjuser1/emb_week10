2bcfe7277a6972504e8a8f60feaf1134945a1c90
jjuser1 <ajsirhzz75@konkuk.ac.kr>
2023-11-22 21:51:33 +0900

    JWcompile

---
+++ .button_driver.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/jjuser/working/emb_week10/button_driver.ko := arm-linux-gnueabihf-ld -r  -EL -z noexecstack   --build-id=sha1  -T scripts/module.lds -o /home/jjuser/working/emb_week10/button_driver.ko /home/jjuser/working/emb_week10/button_driver.o /home/jjuser/working/emb_week10/button_driver.mod.o;  true
---
+++ seg_example_driver.mod
@@ -0,0 +1,1 @@
+/home/jjuser/working/emb_week10/seg_example_driver.o
